f9f0ad7a11b2c9a909ab245d1faa55207cfe8542
Adina Ather <adinaather692@gmail.com>
2026-06-16 19:39:54 +0500

    Update source code files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include "HotelManager.h"
 using namespace std;
 
 int main() {
-    cout << "Hotel System Ready!" << endl;
+    HotelManager myHotel;
+    int choice;
+    
+    // Yahan hum rooms add kar rahe hain
+    myHotel.addRoom(101, ECONOMIC);
+    myHotel.addRoom(102, LUXURY);
+
+    cout << "Welcome to Adina's Hotel System!" << endl;
+    
+    // Simple Menu
+    cout << "1. View All Rooms" << endl;
+    cout << "2. Exit" << endl;
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    if(choice == 1) {
+        myHotel.showAllRooms();
+    }
+
     return 0;
 }
